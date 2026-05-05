ac1ca5df097224b10503fc1a478458ad1dbfaef7
coderwas <khanwasimt.116@gmail.com>
2026-05-05 21:09:16 +0530

    updated fact test, added scd 2 on dim product with tests

--- models/marts/dim__product.sql
+++ models/marts/dim__product.sql
@@ -1,16 +1,23 @@
-WITH
-    product AS (
-        SELECT
-            product_id AS product_key,
-            product_id,
-            product_name,
-            product_line,
-            buy_price,
-            msrp
-        FROM
-            {{ ref('stg__products') }}
+with 
+    snapshot as (
+        select * from {{ ref('dim__product_snapshot') }}
+    ),
+
+    final as (
+        select
+        dbt_scd_id as product_key,
+        product_id,
+        product_name,
+        product_line,
+        buy_price,
+        msrp,
+        date(dbt_valid_from) as valid_from,
+        date(dbt_valid_to) as valid_to,
+        case 
+            when dbt_valid_to is null then 1 
+            else 0 
+        end as is_current
+    from snapshot
     )
-SELECT 
-    *
-FROM 
-    product
+
+select * from final
